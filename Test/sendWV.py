49c570a902abcd9c93608832626e87dafc86ee7c
Harini <hsundararaman@lsst.org>
2018-04-26 11:10:41 -0700

    Added inPosition into the Monochromator Client

--- Test/sendWV.py
+++ Test/sendWV.py
@@ -3,7 +3,7 @@ import time
 import sys 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 50000))
+s.connect(('140.252.33.122', 50000))
 s.settimeout(10)
 
 data = sys.argv[1]
@@ -11,17 +11,27 @@ data = sys.argv[1]
 
 #data = '!WL 450.0'+'\r\n'
 data = data +'\r\n'
-s.sendall(data.encode())
 
+maxDelay = 0.0
 
-time.sleep(0.05)
-print("Message sent")
+for i in range(100000):
+	start = time.time()
+	s.sendall(data.encode())
+	print("Total time send: %d", (time.time() - start))
 
-try:
-	data = s.recv(1024)
-	print(data)
-except socket.error as msg:
-	print(msg)
-	
+	#time.sleep(0.05)
+	print("Message sent")
+
+	try:
+		datar = s.recv(1024)
+		print(datar)
+	except socket.error as msg:
+		print(msg)
+	delay = (time.time() - start)
+	if(i>0 and delay > maxDelay):
+		maxDelay = delay
+	print("Total time end: %d", delay)
+	print("Total max delay end: %d", maxDelay)
+	time.sleep(1)
 s.close()
 print('Sent')
